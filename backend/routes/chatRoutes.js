2766cde1015b599d65bfab31d74d980c121597a4
deshanWimalasooriya <deshanwimalasooriya000@gmail.com>
2026-06-29 11:44:52 +0530

    remove

--- backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChatHistory, saveMessage } = require('../controllers/chatController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/:userId').get(protect, getChatHistory);
-router.route('/').post(protect, saveMessage);
-
-module.exports = router;
--- backend/routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getProducts } = require('../controllers/productController');
-
-router.route('/').get(getProducts);
-
-module.exports = router;
